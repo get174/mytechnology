3e1ab48d5804b58797f36f6db7edbd234643165f
Get Tshibanda <62560899+get174@users.noreply.github.com>
2026-08-18 11:11:52 +0200

    correction du mail pro

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -62,7 +62,7 @@ export default function Contact() {
                 </div>
                 <div>
                   <h3 className="text-xl font-bold text-white mb-2">Email</h3>
-                  <p className="text-slate-300">contact@mytechnology.cd</p>
+                  <p className="text-slate-300">info@mytechnology.cd</p>
                 </div>
               </div>
             </div>
